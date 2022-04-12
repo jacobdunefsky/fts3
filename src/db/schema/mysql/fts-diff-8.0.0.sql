e0fdadd29df6ae17714276fd0ec617e2c2091b79
Joao Lopes <joao.pedro.batista.lopes@cern.ch>
2022-04-12 17:59:29 +0200

    Introduce 'staging_metadata' column in the t_file table

--- src/db/schema/mysql/fts-diff-8.0.0.sql
+++ src/db/schema/mysql/fts-diff-8.0.0.sql
@@ -2,6 +2,7 @@
 -- FTS3 Schema 8.0.0
 -- [FTS-1744] Schema changes for the eviction feature
 -- [FTS-1768] SRM TURL protocol configurable per link
+-- [DMC-1302] Implement HTTP bringonline operation
 --
 
 ALTER TABLE `t_se`
@@ -9,6 +10,12 @@ ALTER TABLE `t_se`
 
 ALTER TABLE `t_link_config`
     ADD COLUMN `3rd_party_turl` varchar(150) DEFAULT NULL;
+
+ALTER TABLE `t_file`
+    ADD COLUMN `staging_metadata` text;
+
+ALTER TABLE `t_file_backup`
+    ADD COLUMN `staging_metadata` text;
 
 INSERT INTO t_schema_vers (major, minor, patch, message)
 VALUES (8, 0, 0, 'FTS-1702: Schema changes for the eviction feature');
